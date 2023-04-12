19f97c920bdbc601f7d375e34b2634f5ae6ab9f9
rahul rawat <golubook09@gmail.com>
2023-04-12 09:16:30 +0100

    responsive grid

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { SimpleGrid,Box } from '@chakra-ui/react'
 function App() {
   return (
     <div className="App">
-    <SimpleGrid columns={4} spacing={10}>
+    <SimpleGrid columns={{sm:2,md:3,lg:4,xl:5}} spacing={2}>
+  <Box bg='tomato' height='80px'></Box>
+  <Box bg='tomato' height='80px'></Box>
+  <Box bg='tomato' height='80px'></Box>
+  <Box bg='tomato' height='80px'></Box>
+  <Box bg='tomato' height='80px'></Box>
   <Box bg='tomato' height='80px'></Box>
   <Box bg='tomato' height='80px'></Box>
   <Box bg='tomato' height='80px'></Box>
