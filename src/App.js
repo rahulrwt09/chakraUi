269f42b63a90478c8d9450d88dccb263d0fcbcbe
rahul rawat <golubook09@gmail.com>
2023-04-12 08:22:56 +0100

    icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
-import { Spinner } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
 function App() {
   return (
     <div className="App">
- <Spinner color='red.500' />
+    
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   
+ <IconButton aria-label='Search database' icon={<i class="fa-solid fa-house"></i>} />
         
     </div>
   );
